78e53b2197a1230195559432247e3f8d8e64212d
MilkMangoTea <blockbyblock@qq.com>
2025-12-10 14:31:37 +0800

    加入抽签功能

--- my_proxy.py
+++ my_proxy.py
@@ -215,7 +215,7 @@ async def qq_bot():
                 1029247118,
             ],
             push_hour=14,
-            push_minute=22,
+            push_minute=33,
             theme="random"
         )
 
